5a4e48f96c3c7803948fa8b98187f10dc74d6b38
Ming-der Wang <ming@log4analytics.com>
2014-12-09 19:49:22 +0800

    do not auto rebuild in script

--- update_website.sh
+++ update_website.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+MAIN_DIR=/Users/mwang/src/zone/61/log4analytics.com/minimal-mistakes/
 SRC_DIR=/Users/mwang/src/zone/61/log4analytics.com/minimal-mistakes/_site
 DES_DIR=/Users/mwang/src/zone/61/log4analytics.com/mingderwang.github.io
 
 echo '----------'
 echo $SRC_DIR
 echo '----------'
+cd $MAIN_DIR
+#bundle exec jekyll build
 rsync -av --delete $SRC_DIR/* $DES_DIR 
 cd $DES_DIR
 git status 
