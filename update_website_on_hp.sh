23ea301d3091fa170f1a77eec7ebd2c3b8a01052
Ming-der Wang <mingder78@yahoo.com>
2015-03-03 12:14:39 +0800

    add script for hp nodebook

---
+++ update_website_on_hp.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+MAIN_DIR=/home/ming-der/src/zone/61/log4analytics.com/minimal-mistakes/
+SRC_DIR=/home/ming-der/src/zone/61/log4analytics.com/minimal-mistakes/_site
+DES_DIR=/home/ming-der/src/zone/61/log4analytics.com/mingderwang.github.io
+
+echo '----------'
+echo $SRC_DIR
+echo '----------'
+cd $MAIN_DIR
+#bundle exec jekyll build
+rsync -av --delete $SRC_DIR/* $DES_DIR 
+cd $DES_DIR
+git status 
+git add .
+echo '----------'
+git status
+git commit -m 'update contents'
+git push
+echo '----------'
+echo $DES_DIR
+echo '----------'
